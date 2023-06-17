a94e55b0b791c8b9aae9e9496779048ccbc485ab
John Pazarzis <jpazarzis@gmail.com>
2023-06-17 15:04:04 -0400

    Modifies the nifti_mri.py.

--- src/nifti_mri.py
+++ src/nifti_mri.py
@@ -1,58 +1,190 @@
+"""Exposes the MRIs as a collection of objects."""
+
 import json
 import os.path
 
 import cv2
 import nibabel as nib
-import psycopg2
 
 import cogni_scan.src.dbutil as dbutil
 
-
-
-UPDATE_SQL = """
-    Update 
+_SQL_UPDATE_ONE = """
+    Update
         Scan set skipit={skipit}, axis='{axis}', rotation='{rotation}'
     where
         fullpath='{fullpath}'
 """.format
 
-SELECT_SQL = """
-    Select axis, rotation 
-    from scan 
-    where fullpath = '{fullpath}';
-""".format
-
-
-SQL_SELECT_ALL = """
-select 
-    scan_id, fullpath, days, patiend_id, origin, 
-    skipit, health_status, axis, rotation 
-from 
+_SQL_SELECT_ALL = """
+select
+    scan_id, fullpath, days, patiend_id, origin,
+    skipit, health_status, axis, rotation
+from
     scan
 order by patiend_id, days
 """
 
-
-def loadFolded():
-    mris = [NiftiMri(*row) for row in dbutil.execute_query(SQL_SELECT_ALL)]
-    folded = {}
-    for mri in mris:
-        patiend_id = mri.getPatientID()
-        if patiend_id not in folded:
-            folded[patiend_id] = []
-        folded[patiend_id].append(mri)
-    return folded
+_SQL_SELECT_EXIT_HEALTH_STATUS = """
+select a.patient_id, a.health_status from diagnosis a, 
+(Select patient_id, max(days) as days from diagnosis group by patient_id) b  
+where a.patient_id = b.patient_id and a.days = b.days;
+"""
 
 
-class NiftiMri:
+def int2HealthStatus(value):
+    if value == 0:
+        return "H"
+    elif value == 1:
+        return "U"
+    elif value == 2:
+        return "D"
+    else:
+        return "?"
 
-    def __init__(self, scan_id, fullpath, days, patiend_id,
+
+class PatientCollection:
+    """Holds all the available MRI objects.
+
+    Attributes
+
+    __patients: Maps patient IDs to Patient objects.
+    __mri_id_to_mri: Maps scan IDs to corresponding Scan objects.
+    __was_loaded: a flag indicating whether the data was successfully loaded.
+    """
+
+    def __init__(self):
+        """Initialize the object with default values."""
+        self.__patients = None
+        self.__mri_id_to_mri = None
+        self.__was_loaded = False
+
+    def _loadFromDb(self):
+        """Load data from the database and populate the patient collection.
+
+        Each row from the database is processed to create a new Scan object,
+        which is then associated with the corresponding Patient. Additionally,
+        a mapping from Scan ID to Scan object is created for faster searches.
+        """
+        self.__patients = {}
+        self.__mri_id_to_mri = {}
+        self.__was_loaded = False
+
+        for row in dbutil.execute_query(_SQL_SELECT_ALL):
+            (scan_id, fullpath, days, patient_id, origin,
+             skipit, health_status, axis, rotation) = row
+
+            if patient_id not in self.__patients:
+                self.__patients[patient_id] = Patient(patient_id)
+
+            scan = Scan(scan_id, fullpath, days, patient_id, origin, skipit,
+                        health_status, axis, rotation)
+
+            self.__patients[patient_id].addScan(scan)
+            self.__mri_id_to_mri[scan.getScanID()] = scan
+
+        # Assign the exit health status to all the patients.
+        for row in dbutil.execute_query(_SQL_SELECT_EXIT_HEALTH_STATUS):
+            patient_id, health_status = row
+            if patient_id in self.__patients:
+                self.__patients[patient_id].setExitHealthStatus(health_status)
+
+        self.__was_loaded = True
+
+    def getPatient(self, patiend_id):
+        if not self.__was_loaded:
+            self._loadFromDb()
+        assert self.__was_loaded
+        if patiend_id in self.__patients:
+            return self.__patients[patiend_id]
+        else:
+            raise ValueError
+
+    def getPatientIDs(self):
+        """Yields patient IDs from the patient collection."""
+        if not self.__was_loaded:
+            self._loadFromDb()
+        assert self.__was_loaded
+        for patiend_id, patient in self.__patients.items():
+            yield patiend_id, patient.getTitle()
+
+    def getMrisByPatient(self, patient_id):
+        if not self.__was_loaded:
+            self._loadFromDb()
+        assert self.__was_loaded
+        assert patient_id in self.__patients
+        patient = self.__patients[patient_id]
+        count = patient.numberOfScans()
+        for i in range(count):
+            yield patient.getScan(i)
+
+    def getMriByMriID(self, mri_id):
+        assert mri_id in self.__mri_id_to_mri
+        return self.__mri_id_to_mri[mri_id]
+
+    def getDesctiptiveDataForPatient(self, patient_id):
+        pass
+
+
+class Patient:
+    """Holds the information about a patient.
+
+    Allows us to sort / search for patients for specific health statuses,
+    origin of Mris (like Oasis3, Oasis2, ADNI etc).
+
+    Attributes
+
+    __label: Labels the patient with a string of two letters in
+    the format AB where A and B represent the health status for the patient
+    as he entered and exited the system.
+
+    H: healthy
+    D: demented
+    U: Uncertain.
+    """
+
+    def __init__(self, patient_id):
+        self.__patient_id = patient_id
+        self.__scans = []
+        self.__exit_health_status = '?'
+
+    def addScan(self, scan):
+        self.__scans.append(scan)
+        self.__scans.sort(key=lambda x: x.getDays())
+
+    def getTitle(self):
+        return f'{self.__patient_id} {self.getLabel()}'
+
+    def getExitHealthStatus(self):
+        return self.__exit_health_status
+
+    def setExitHealthStatus(self, health_status):
+        assert 0 <= health_status <= 2
+        self.__exit_health_status = health_status
+
+    def getLabel(self):
+        exit_status = int2HealthStatus(self.__exit_health_status)
+        enter_status = int2HealthStatus(self.__scans[0].getHealthStatus())
+        label = f'{enter_status}' \
+                f'{exit_status}'
+        return label
+
+    def numberOfScans(self):
+        return len(self.__scans)
+
+    def getScan(self, index):
+        assert 0 <= index < len(self.__scans)
+        return self.__scans[index]
+
+
+class Scan:
+
+    def __init__(self, scan_id, fullpath, days, patient_id,
                  origin, skipit, health_status, axis, rotation):
         self.__scan_id = scan_id
         self.__img = None
         self.__filepath = fullpath
         self.__days = days
-        self.__patient_id = patiend_id
+        self.__patient_id = patient_id
         self.__origin = origin
         self.__skipit = skipit
         self.__health_status = health_status
@@ -67,7 +199,7 @@ class NiftiMri:
     def saveToDb(self):
         axis = json.dumps(self.__axis_mapping)
         rotation = json.dumps(self.__rotation)
-        sql = UPDATE_SQL(
+        sql = _SQL_UPDATE_ONE(
             skipit=self.__skipit,
             axis=axis,
             rotation=rotation,
@@ -76,7 +208,7 @@ class NiftiMri:
         dbutil.execute_non_query(sql)
         self.__is_dirty = False
 
-    def getPatientDesc(self, patient_id):
+    def getPatientDesc(self):
         """Returns the descritive data for the patiend."""
 
     def getScanID(self):
@@ -86,7 +218,8 @@ class NiftiMri:
         return self.__skipit
 
     def getMriID(self):
-        return f"{self.__scan_id}:{self.__days:5}:{self.getHealthStatus()}"
+        hs = int2HealthStatus(self.getHealthStatus())
+        return f"{self.__scan_id}:{self.__days:5}:{hs}"
 
     def getOrigin(self):
         return self.__origin
@@ -97,19 +230,6 @@ class NiftiMri:
     def getFilePath(self):
         return self.__filepath
 
-    def getPatientID(self):
-        return self.__patient_id
-
-    def getCaption(self):
-        return f'{self.__patient_id}:{self.__days}'
-
-    def _loadFromDb(self):
-        sql = SELECT_SQL(fullpath=self.__filepath)
-        for row in dbutil.execute_query(sql):
-            axis = row[0]
-            self.__axis_mapping = {int(k): v for k, v in axis.items()}
-            self.__rotation = row[1]
-
     def isDirty(self):
         return self.__is_dirty
 
@@ -117,14 +237,7 @@ class NiftiMri:
         return self.__filepath
 
     def getHealthStatus(self):
-        if self.__health_status == 0:
-            return "healthy"
-        elif self.__health_status == 1:
-            return "uncertain"
-        elif self.__health_status == 2:
-            return "demented"
-        else:
-            return "unknown"
+        return int(self.__health_status)
 
     def changeOrienation(self, axis):
         assert 0 <= axis <= 2
@@ -149,11 +262,11 @@ class NiftiMri:
             raise ValueError
         if not len(axis_mapping) == 3:
             raise ValueError
-        if not 0 in axis_mapping:
+        if 0 not in axis_mapping:
             raise ValueError
-        if not 1 in axis_mapping:
+        if 1 not in axis_mapping:
             raise ValueError
-        if not 2 in axis_mapping:
+        if 2 not in axis_mapping:
             raise ValueError
 
         self.__axis_mapping = {
@@ -199,11 +312,18 @@ class NiftiMri:
 
 
 if __name__ == '__main__':
-    mris = loadFolded()
-    for x in mris:
-        print(x)
+    patients = PatientCollection()
+    # ids = list(patients.getPatientIDs())
+    # for x in ids:
+    #     p = patients.getPatient(x[0])
+    #     label = p.getLabel()
+    #     if label == 'HD':
+    #          print(p.getTitle())
 
+    patient_id = 'OAS30326'
 
-    # f = "/home/john/repos/cogni_scan/src/impl/tests/testing_data/mri-1.nii.gz"
-    # nm = NiftiMri(f)
-    # nm.saveToDb()
+    patient = patients.getPatient(patient_id)
+    print(patient.getLabel())
+
+    for x in patients.getMrisByPatient(patient_id):
+        print(x.getMriID())
